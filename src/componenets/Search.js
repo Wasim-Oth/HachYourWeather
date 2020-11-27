fd070cb1eeb7feb5194cb9214969f39677c2f3e6
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-11-27 18:16:33 +0100

    updated

--- src/componenets/Search.js
+++ src/componenets/Search.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Search = (props) => {
-    return (
-        <div className='search'>
-             <input type='text' onChange={props.Change} placeholder='Search City' />
-             <button onClick= {props.Fetch} > Search </button> 
-        </div>
-       
-    )
-}
+  return (
+    <div className="search">
+      <input type="text" onChange={props.Change} placeholder="Search City" />
+      <button onClick={props.Fetch}> Search </button>
+    </div>
+  );
+};
 
 export default Search;
